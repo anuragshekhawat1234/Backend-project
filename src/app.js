5b1c68c3cff4bdf4b1f441b3e983e67a94d75db0
Anurag shekhawat <anuragshekhawat1849@gmail.com>
2026-01-26 17:18:23 +0530

    added controllerand routers

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ app.use(express.urlencoded({
 }))
 app.use(express.static("public"))
 app.use(cookieParser())
+// Routes import 
+
+import userRouter from "./routes/user.routes.js"
 
 
-export { app }
+
+// routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+
+export default app;
