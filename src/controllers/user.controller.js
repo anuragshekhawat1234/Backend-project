f5618925eb9f97994fafff6f7a322665a268b517
Anurag shekhawat <anuragshekhawat1849@gmail.com>
2026-01-27 22:12:19 +0530

    added logic

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,16 @@
 import { asynchandler } from "../utils/asynchandler.js";
 const registerUser = asynchandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+    // get user details from frontend 
+    // validation - not empty
+    // check if user already exists
+    // check for images, check for avatar
+    // upload them to cloudinary
+    // create user object - create entry in db
+    // remove password and refresh token from response
+    // check for user creation
+    // return response 
+
+    const { fullName, email, username, password } = req.body
+    console.log("email:", email);
 })
 export { registerUser }
